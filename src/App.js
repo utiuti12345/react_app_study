c96b6defba7d29acfe4d67805e6a098c9d0bc4e9
Yuta Saotome <private.uta.sao@gmail.com>
2020-09-17 22:27:26 +0900

    study react 3-4 3-31

--- src/App.js
+++ src/App.js
@@ -2,74 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let data = {title:'Title',message:'this is sample message'};
+
+const SampleContext = React.createContext(data);
+
 class App extends React.Component{
-    input = '';
-
-    msgStyle = {
-        fontSize:"20pt",
-        color:"#900",
-        margin:"20px 0px",
-        padding:"5px",
-    };
-
-    inputStyle = {
-        fontSize:"20pt",
-        padding:"5px",
-    };
-
-    constructor(props){
-        super(props);
-        this.state = {
-            message:"type your name."
-        };
-        this.doCheck = this.doCheck.bind(this);
-    }
-
-    doChange(e){
-        this.input = e.target.value;
-    }
-
-    doSubmit(e){
-        this.setState({
-            message:"Hello, " + this.input + "!"
-        });
-        e.preventDefault();
-    }
-
-    doCheck(e){
-        alert(e.target.value + "は長すぎます。(最大10文字)");
-    }
+    newdata = {title:'新しいタイトル',message:'新しいメッセージです。'};
 
     render() {
         return <div>
             <h1>React</h1>
-            <h2>{this.state.message}</h2>
-            <Message maxlength="10" onCheck={this.doCheck} />
+            <Title/>
+            <Message/>
+            <SampleContext.Provider value={this.newdata}>
+                <Title/>
+                <Message/>
+            </SampleContext.Provider>
         </div>
     }
 }
 
-class Message extends React.Component {
-    inputStyle = {
-        fontSize:"20pt",
-        padding:"5px",
-    };
-
-    constructor(props){
-        super(props);
-        this.doChange = this.doChange.bind(this);
-    };
-
-    doChange(e){
-        if(e.target.value.length > this.props.maxlength){
-            console.log(this.props.onCheck);
-            this.props.onCheck(e);
-            e.target.value = e.target.value.substr(0,this.props.maxlength);
-        }
-    }
+class Title extends React.Component{
+    static contextType = SampleContext;
 
     render() {
-        return <input type="text" style={this.inputStyle} onChange={this.doChange}/>
+        return(
+            <div>
+                <h2>{this.context.title}</h2>
+            </div>
+        )
+    }
+}
+
+class Message extends React.Component{
+    static contextType = SampleContext;
+
+    render() {
+        return(
+            <div>
+                <p>{this.context.message}</p>
+            </div>
+        )
     }
 }
 
