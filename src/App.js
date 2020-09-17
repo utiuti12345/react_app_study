6cab989b4d077177d5e04e3de7ef2cb7b97ff795
Yuta Saotome <private.uta.sao@gmail.com>
2020-09-18 00:32:43 +0900

    study react redux 4-1 4-1

--- src/App.js
+++ src/App.js
@@ -1,49 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {connect} from "react-redux";
 
-let data = {title:'Title',message:'this is sample message'};
-
-const SampleContext = React.createContext(data);
+// ステートのマッピング
+function mappingState(state) {
+    console.log(state.message);
+    return state;
+}
 
 class App extends React.Component{
-    newdata = {title:'新しいタイトル',message:'新しいメッセージです。'};
+
+    constructor(props){
+        super(props)
+    }
 
     render() {
         return <div>
             <h1>React</h1>
-            <Title/>
             <Message/>
-            <SampleContext.Provider value={this.newdata}>
-                <Title/>
-                <Message/>
-            </SampleContext.Provider>
+            <Button/>
         </div>
     }
 }
 
-class Title extends React.Component{
-    static contextType = SampleContext;
+// ストアのコネクト
+// let wrapWithConnet = connect();
+// App = wrapWithConnet(App);
+App = connect()(App);
 
-    render() {
-        return(
-            <div>
-                <h2>{this.context.title}</h2>
-            </div>
-        )
-    }
-}
-
+// メッセージ表示コンポーネント
 class Message extends React.Component{
-    static contextType = SampleContext;
+    style = {
+        fontSize:"10pt",
+        padding:"20px 5px",
+    };
 
     render() {
         return(
-            <div>
-                <p>{this.context.message}</p>
-            </div>
+            <p style={this.style}>
+                {this.props.message}:{this.props.counter}
+            </p>
+           )
+    }
+}
+
+// ストアのコネクト
+Message = connect(mappingState)(Message);
+
+// ボタン表示のコンポーネント
+class Button extends React.Component{
+    style = {
+        fontSize:"16pt",
+        padding:"5px 10px",
+    };
+
+    constructor(props){
+        super(props);
+        this.doAction = this.doAction.bind(this);
+    }
+
+    // ボタンクリックでディスパッチを実行
+    doAction(e){
+        if(e.shiftKey){
+            this.props.dispatch({type:'DECREMENT'});
+        }else{
+            this.props.dispatch({type:'INCREMENT'});
+        }
+    }
+
+    render() {
+        return(
+            <button style={this.style}
+                onClick={this.doAction}>
+                click
+            </button>
         )
     }
 }
+
+// ストアのコネクト
+Button = connect()(Button);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from "redux";
+import Provider from "react-redux/es/components/Provider";
+
+// ステートの値
+let state_value = {
+    counter: 0,
+    message: "COUNTER",
+};
+
+// レデューサー
+function counter(state = state_value, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1,
+                message: "INCREMENT"
+            };
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1,
+                message: "DECREMENT"
+            };
+        default:
+            return state
+    }
+};
+
+// ストアを作成
+let store = createStore(counter);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
